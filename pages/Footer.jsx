6755e88e8913f2f05aa822ba3df59e73062fa329
Staicy Stadtler <staicy@hotmail.de>
2023-09-21 13:07:03 +0200

    looping footer

--- pages/Footer.jsx
+++ pages/Footer.jsx
@@ -3,7 +3,14 @@ import "./Footer.css";
 export function Footer() {
   return (
     <>
-      <p className="footer">Copyright by Staicy 👩‍💻</p>
+      <p className="footer">
+        Copyright by Staicy <br />
+        <img
+          className="loop"
+          src="/public/female-technologist.png"
+          alt="coding lady emoji"
+        />
+      </p>
     </>
   );
 }
--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -1,8 +1,51 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./Homepage.css";
+import { Link } from "react-router-dom";
+
 export function Homepage() {
+  const [randomMovie, setRandomMovie] = useState();
+  const movieUrl =
+    "https://2zc6fti416.execute-api.eu-central-1.amazonaws.com/prod/movies/";
+  useEffect(() => {
+    axios
+      .get(movieUrl)
+      .then(({ data }) => {
+        const randomIndex = Math.floor(Math.random() * data.length);
+        console.log(randomIndex);
+        return axios.get(movieUrl + data[randomIndex].id);
+      })
+      .then(({ data }) => {
+        setRandomMovie(data);
+      })
+      .catch(() => {
+        console.log("catching the error 🐛");
+      })
+      .finally(() => {
+        console.log("Request is done 🦋");
+      });
+  }, []);
+
+  if (!randomMovie) {
+    return <p>Loading</p>;
+  }
+
   return (
     <>
       <section>
-        <h3>I am the Homepage 🏡</h3>
+        <h1>Welcome to the Ultimate Movie Guide</h1>
+        <figure className="random-movie">
+          <Link to={`movies/` + randomMovie.id}>
+            <img
+              src={randomMovie.image}
+              alt={randomMovie.title}
+              className="random-movie"
+            />
+          </Link>
+          <figcaption>
+            Our highlighted movie today is {randomMovie.title}
+          </figcaption>
+        </figure>
       </section>
     </>
   );
